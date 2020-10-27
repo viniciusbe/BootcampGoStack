e309dce2cfc5468ee06209726d04f29f51468bc6
Vinícius Santos <vinicius.bernardesds@gmail.com>
2020-10-27 13:15:50 -0300

    Week 1 : Insomnia, app, middleware

--- aulas/conceitos-dev/backend/src/index.js
+++ aulas/conceitos-dev/backend/src/index.js
@@ -1,10 +1,96 @@
 const { request, response } = require('express')
 const express = require('express')
+const { uuid, isUuid } = require('uuidv4')
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' })
+app.use(express.json())
+
+function logRequests(request, response, next) {
+
+    const { method, url } = request
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`
+
+    console.time(logLabel)
+
+    next()
+
+    console.timeEnd(logLabel)
+
+}
+
+function validateProjectId(request, response, next) {
+    const { id } = request.params
+
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid project ID' })
+    }
+
+    return next()
+}
+
+app.use(logRequests)
+app.use('/projects/:id', validateProjectId)
+
+const projects = []
+
+app.get('/projects', (request, response) => {
+
+    const { title } = request.query
+
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects
+    return response.json(results)
 })
 
-app.listen(3333)
+app.post('/projects', (request, response) => {
+
+    const { title, name } = request.body
+
+    const project = { id: uuid(), title, name }
+
+    projects.push(project)
+
+    return response.json(project)
+})
+
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params
+    const { title, name } = request.body
+
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found' })
+    }
+
+    const project = {
+        id,
+        title,
+        name
+    }
+
+    projects[projectIndex] = project
+
+    return response.json(project)
+})
+
+app.delete('/projects/:id', (request, response) => {
+    const { id } = request.params
+
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found' })
+    }
+
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
+})
+
+app.listen(3333, () => {
+    console.log('Back-end started 🛴')
+})
