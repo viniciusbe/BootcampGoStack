6d33eb5e1d95514d401c734793f89774c63c3f06
viniciusbe <vinicius.bernardesds@gmail.com>
2021-02-03 18:57:03 -0300

    update: provider's response serialized

--- aulas/iniciando-back-end/src/modules/appointments/services/ListProvidersService.ts
+++ aulas/iniciando-back-end/src/modules/appointments/services/ListProvidersService.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -30,7 +31,7 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users));
     }
 
     return users;
